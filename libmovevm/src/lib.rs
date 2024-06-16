f1c2663dd8eb0b4051ab84341d6b5a8528d425b5
beer-1 <147697694+beer-1@users.noreply.github.com>
2024-06-16 15:49:28 +0900

    enable leak report

--- libmovevm/src/lib.rs
+++ libmovevm/src/lib.rs
@@ -7,7 +7,7 @@ static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
 #[allow(non_upper_case_globals)]
 #[export_name = "malloc_conf"]
-pub static malloc_conf: &[u8] = b"prof:true,prof_active:true,lg_prof_sample:19\0";
+pub static malloc_conf: &[u8] = b"prof:true,prof_active:true,lg_prof_sample:19,prof_leak:true\0";
 
 mod api;
 mod args;
